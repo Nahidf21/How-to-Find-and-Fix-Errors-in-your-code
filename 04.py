3cd183c2e834ad8816b7bd7e39c0529644fbed44
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-24 23:34:00 -0500

    print is your friend
    it will print zero

---
+++ 04.py
@@ -0,0 +1,7 @@
+#print is your friend 
+pages=0
+word_per_page=0
+pages=int(input("Number of the pages: "))
+word_per_page== int(input("Number of words per page: "))
+total_words=pages*word_per_page
+print(total_words)
