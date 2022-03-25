640e1a7caf006ef7bfd01d23dff0e8c4cd360bc6
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-24 23:22:10 -0500

    play computer

---
+++ 03.py
@@ -0,0 +1,7 @@
+#play computer
+year=int(input("Whats your year of birth?"))
+#if year> 1980 and year<1994:
+if year> 1980 and year<=1994:
+    print("You are a millenial.")
+elif year>1994:
+    print("You are a Gen Z.")
