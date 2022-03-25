dc76a1869c05e5ae7607731c3380f46d950dcf48
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-24 23:38:52 -0500

    print is your friend
    Solution  : #word_per_page== int(input("Number of words per page: ")) "its a condition True or False "
    word_per_page= int(input("Number of words per page: "))

--- 04.py
+++ 04.py
@@ -2,6 +2,7 @@
 pages=0
 word_per_page=0
 pages=int(input("Number of the pages: "))
-word_per_page== int(input("Number of words per page: "))
+#word_per_page== int(input("Number of words per page: ")) "its a condision True or False "
+word_per_page= int(input("Number of words per page: "))
 total_words=pages*word_per_page
 print(total_words)
