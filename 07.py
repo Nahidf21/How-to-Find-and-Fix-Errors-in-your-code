df5dee95e38d1ca0a600c02b963bde3efb40a1f8
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-27 16:17:28 -0500

    Leap year
    find the error and solve the leap year operation

--- 07.py
+++ 07.py
@@ -1,4 +1,12 @@
-year=input("Which year do you wnat to check? ")
+year=int(input("Which year do you wnat to check? "))
 #first it should be devided by 4 and no  remaining 
 if year %4==0:
-    if year %
+    if year %100==0:
+        if year %400==0:
+            print("Its Leap Year")
+        else:
+            print("Its not Leap Year")
+    else: 
+        print("Its a Leap year")
+else:
+    print("Its not a leap year")     
